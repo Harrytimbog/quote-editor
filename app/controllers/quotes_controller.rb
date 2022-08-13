1938dad2bb2f6a18e2528cef5bc9b2988424edad
Timilehin Arigbede <Harriemannie@gmail.com>
2022-08-13 08:50:50 +0100

    Flash messages with Turbo in Rails 7

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -30,19 +30,21 @@ class QuotesController < ApplicationController
   
     def update
       if @quote.update(quote_params)
-        redirect_to quotes_path, notice: "Quote was successfully updated."
+        respond_to do |format|
+          format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
+          format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+        end
       else
-        # Add `status: :unprocessable_entity` here
         render :edit, status: :unprocessable_entity
       end
     end
-  
+      
     def destroy
       @quote.destroy
     
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
       end
     end
       
